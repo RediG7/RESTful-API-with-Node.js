5fecdd730b9db31d756d1aa945d65eea534a3c0c
RediG7 <guleksiredi7@gmail.com>
2020-12-23 11:55:19 +0100

    Add body parser, handle CORS errors

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
 app.use(morgan('dev')); // HTTP request logger middleware for node.js
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// adding before any response happens, second param is to give access (*) in this case to all clients
+app.use((req, res, next) => {
+    res.header("Access-Conntrol-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    // browser always sends a OPTIONS request first before making the post or put req 
+    // browser sees if it is allowed to make the req
+    if(req.method === 'OPTIONS'){
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        res.status(200).json({});
+    }
+    next();
+});
 
 // middleware, routes which should handle requests
 app.use("/products", productRoutes);
