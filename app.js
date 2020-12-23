0708f38539af84a8081e2d1fd7b1012bd69005ce
RediG7 <guleksiredi7@gmail.com>
2020-12-23 11:21:13 +0100

    Add automatic reloading, default logging, error handling

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
-
+const morgan = require("morgan");
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
-// middleware
+
+app.use(morgan('dev')); // HTTP request logger middleware for node.js
+
+// middleware, routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
