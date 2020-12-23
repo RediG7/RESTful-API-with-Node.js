4add320cf42f29db78a0e50f2285c691c5a7a928
RediG7 <guleksiredi7@gmail.com>
2020-12-23 11:58:52 +0100

    Create order object

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+// Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: "Orders were fetched"
@@ -8,8 +9,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(201).json({
-        message: "Orders was created"
+        message: "Orders was created",
+        order: order
     });
 });
 
